639ba67da527b82d216c4f330d88c1b390d8a984
mehdikeshavarz <mehdi2019keshavarz@gmail.com>
2023-12-06 08:44:07 +0330

    start project

---
+++ app/src/main/java/project/G.java
@@ -0,0 +1,15 @@
+package project;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.content.Context;
+
+public class G extends Application {
+    @SuppressLint("StaticFieldLeak")
+    public static Context context;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+}
---
+++ app/src/main/java/project/MainActivity.java
@@ -0,0 +1,16 @@
+package project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.keshavarz.mehdi.dotline.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
