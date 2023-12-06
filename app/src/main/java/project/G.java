9853b99f42bce3bef74a7a6ff053ff844b9c802e
mehdikeshavarz <mehdi2019keshavarz@gmail.com>
2023-12-06 11:15:10 +0330

    drawing dots

--- app/src/main/java/project/G.java
+++ app/src/main/java/project/G.java
@@ -3,13 +3,17 @@ package project;
 import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
 
 public class G extends Application {
     @SuppressLint("StaticFieldLeak")
     public static Context context;
+
+
     @Override
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
+        Log.i("LOG", "onCreate From G");
     }
 }
--- app/src/main/java/project/GameView.java
+++ app/src/main/java/project/GameView.java
@@ -3,12 +3,21 @@ package project;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
+    private Paint paint;
+    private int cols = 5;
+    private int rows = 6;
+    private int offsetY;
+    private int offsetX;
+    private int space = 150;
+    private int radius = 15;
     public GameView(Context context) {
         super(context);
         initialize();
@@ -24,11 +33,34 @@ public class GameView extends View {
         initialize();
     }
 
-    private void initialize(){}
+    private void initialize(){
+        paint = new Paint();
+        paint.setColor(Color.WHITE);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setAntiAlias(true);
+
+        int boxWidth= (cols -1) * space;
+        int boxHeight= (rows -1) * space;
+
+
+      DisplayMetrics dp =  G.context.getResources().getDisplayMetrics();
+      int screenHeight =dp.heightPixels;
+      int screenWidth =dp.widthPixels;
+
+        offsetX = (screenWidth - boxWidth) /2;
+        offsetY = (screenHeight - boxHeight) /2;
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawColor(Color.parseColor("#222222"));
+        for (int i =0; i<cols; i++){
+            for (int j=0; j<rows; j++){
+                int x = offsetX +(i * space);
+                int y = offsetY + (j * space);
+                canvas.drawCircle(x, y, radius,paint);
+            }
+        }
     }
 }
