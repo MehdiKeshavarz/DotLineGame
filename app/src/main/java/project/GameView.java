414f8fbf4f7a4aa9ebcff4587f1c2967f120c1b8
mehdikeshavarz <mehdi2019keshavarz@gmail.com>
2023-12-07 10:54:00 +0330

    connect the dots

--- app/src/main/java/project/GameView.java
+++ app/src/main/java/project/GameView.java
@@ -11,13 +11,29 @@ import android.view.View;
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
-    private Paint paint;
-    private int cols = 5;
-    private int rows = 6;
+
+    private static class Point{
+        public int x;
+        public int y;
+
+        Point(int x , int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+
+    private Paint dotPaint;
+    private Paint textPaint;
+    private Paint linePaint;
+
+    private int cols = 4;
+    private int rows = 4;
     private int offsetY;
     private int offsetX;
     private int space = 150;
     private int radius = 15;
+
     public GameView(Context context) {
         super(context);
         initialize();
@@ -34,10 +50,23 @@ public class GameView extends View {
     }
 
     private void initialize(){
-        paint = new Paint();
-        paint.setColor(Color.WHITE);
-        paint.setStyle(Paint.Style.FILL);
-        paint.setAntiAlias(true);
+        dotPaint = new Paint();
+        dotPaint.setColor(Color.WHITE);
+        dotPaint.setStyle(Paint.Style.FILL);
+        dotPaint.setAntiAlias(true);
+
+        linePaint = new Paint();
+        linePaint.setColor(Color.parseColor("#4444ff"));
+        linePaint.setStyle(Paint.Style.FILL);
+        linePaint.setAntiAlias(true);
+        linePaint.setStrokeWidth(10);
+
+        textPaint = new Paint();
+        textPaint.setColor(Color.WHITE);
+        textPaint.setStyle(Paint.Style.FILL);
+        textPaint.setTextSize(30);
+        textPaint.setTextAlign(Paint.Align.CENTER);
+        textPaint.setAntiAlias(true);
 
         int boxWidth= (cols -1) * space;
         int boxHeight= (rows -1) * space;
@@ -55,12 +84,45 @@ public class GameView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawColor(Color.parseColor("#222222"));
+
+        connect(canvas,0, 2,1,2);
+        connect(canvas,2, 2,3,1);
         for (int i =0; i<cols; i++){
             for (int j=0; j<rows; j++){
-                int x = offsetX +(i * space);
-                int y = offsetY + (j * space);
-                canvas.drawCircle(x, y, radius,paint);
+                Point point = computePoint(i, j);
+                canvas.drawCircle(point.x, point.y, radius, dotPaint);
             }
         }
+        debugNaming(canvas);
+
+
+    }
+
+    private void connect(Canvas canvas, int i1, int j1,int i2, int j2){
+        float c =(float)Math.sqrt(Math.pow(i1 - i2,2) + Math.pow(j1 - j2,2));
+        if(c != 1){
+            return;
+        }
+
+        Point p1 = computePoint(i1, 2);
+        Point p2 = computePoint(i2,j2);
+        canvas.drawLine(p1.x, p1.y, p2.x,p2.y, linePaint);
+    }
+
+
+    private void debugNaming(Canvas canvas){
+        for (int i =0; i<cols; i++){
+            for (int j=0; j<rows; j++) {
+                Point point = computePoint(i, j);
+                String name = "" + i + "," +j;
+                canvas.drawText(name,point.x, point.y + 50,textPaint);
+            }
+        }
+    }
+    private Point computePoint(int i , int j){
+        int x = offsetX +(i * space);
+        int y = offsetY + ((rows -1 - j) * space);
+
+        return new Point(x, y);
     }
 }
