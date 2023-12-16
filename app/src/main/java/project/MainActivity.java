0977e1c6384cdec69af2b134c9170603791ed9b9
mehdikeshavarz <mehdi2019keshavarz@gmail.com>
2023-12-16 11:41:17 +0330

    create button rest game

--- app/src/main/java/project/MainActivity.java
+++ app/src/main/java/project/MainActivity.java
@@ -3,6 +3,8 @@ package project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.keshavarz.mehdi.dotline.R;
 
@@ -12,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       Button btnRestGame = findViewById(R.id.btn_rest);
+       final  GameView gameView = findViewById(R.id.game_view);
+
+       btnRestGame.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               gameView.rest();
+           }
+       });
     }
 }
